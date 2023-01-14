6f3dbeec9a6af75b25f1c7bebedde547d10cddf5
yuan8ba <41765558+yuan8ba@users.noreply.github.com>
2023-01-14 15:22:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /app/
 chmod 777 ngrok
-#nohup ./frpc &
-nohup ./ngrok start ssh1 --config config.yml &
+nohup ./frpc &
+#nohup ./ngrok start ssh1 --config config.yml &
 /app/v2ray run
